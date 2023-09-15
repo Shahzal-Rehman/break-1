5fcc73a4dcb3be43c32e14e97feeaca4090d2555
Shahzal Rehman <119114795+SR123456789-maker@users.noreply.github.com>
2023-09-15 18:08:19 +0500

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 const countdownElement = document.getElementById('countdown');
+const welcomeMessage = document.getElementById('welcome-message');
 let seconds = 20;
+
+function disableInput() {
+    // Disable user input by adding a transparent div covering the entire page
+    const overlay = document.createElement('div');
+    overlay.style.position = 'fixed';
+    overlay.style.top = '0';
+    overlay.style.left = '0';
+    overlay.style.width = '100%';
+    overlay.style.height = '100%';
+    overlay.style.backgroundColor = 'transparent';
+    overlay.style.zIndex = '9999';
+    document.body.appendChild(overlay);
+}
+
+function enableInput() {
+    // Remove the transparent overlay to enable user input
+    const overlay = document.querySelector('div');
+    if (overlay) {
+        document.body.removeChild(overlay);
+    }
+}
 
 function updateCountdown() {
     countdownElement.textContent = seconds;
@@ -7,19 +29,13 @@ function updateCountdown() {
 
     if (seconds < 0) {
         clearInterval(timer);
-        document.body.style.backgroundColor = '#000'; // Reset background color
-       if (seconds < 0) {
-    clearInterval(timer);
-    document.body.style.backgroundColor = '#000';
-    // Show the welcome message when the timer reaches 0
-    welcomeMessage.style.display = 'block';
-    // Disable user input during the break
-    disableInput();
-    // Enable user input after the break (e.g., after 20 seconds)
-    setTimeout(enableInput, 20000); // 20 seconds
-    return; // Leave this return statement here
-}
-
+        document.body.style.backgroundColor = '#000';
+        // Show the welcome message when the timer reaches 0
+        welcomeMessage.style.display = 'block';
+        // Disable user input during the break
+        disableInput();
+        // Enable user input after the break (e.g., after 20 seconds)
+        setTimeout(enableInput, 20000); // 20 seconds
         return;
     }
 
@@ -28,15 +44,4 @@ function updateCountdown() {
 }
 
 updateCountdown();
-const timer = setInterval(updateCountdown, 1000);
-
-        return;
-    }
-
-    // Adjust background brightness
-    const brightness = (100 - seconds * 5) + '%';
-    document.body.style.backgroundColor = `hsl(0, 0%, ${brightness})`;
-}
-
-updateCountdown(); // Initialize the countdown
 const timer = setInterval(updateCountdown, 1000);
