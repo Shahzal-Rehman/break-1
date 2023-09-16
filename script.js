93d261c09e0e73ae49447c45eb0061322f2191af
Shahzal Rehman <119114795+SR123456789-maker@users.noreply.github.com>
2023-09-16 17:07:57 +0500

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let timer;
 let isRunning = false;
-let seconds = 0;
+let seconds = 1200; // 20 minutes in seconds (20 minutes * 60 seconds)
 
 function formatTime(seconds) {
     const hours = Math.floor(seconds / 3600);
@@ -11,51 +11,18 @@ function formatTime(seconds) {
 
 function startTimer() {
     if (!isRunning) {
-        const userInput = prompt('Enter timer duration in seconds:');
-        if (userInput !== null && !isNaN(userInput) && userInput > 0) {
-            seconds = parseInt(userInput);
-            timer = setInterval(function () {
-                seconds--;
-                document.getElementById('countdown').textContent = formatTime(seconds);
-                if (seconds <= 0) {
-                    clearInterval(timer);
-                    isRunning = false;
-                    document.getElementById('startButton').textContent = 'Start';
-                    enableInput(); // Enable user input after the timer finishes
-                }
-            }, 1000);
-            isRunning = true;
-            document.getElementById('startButton').textContent = 'Pause';
-            disableInput(); // Disable user input when the timer starts
-        } else {
-            alert('Invalid input. Please enter a valid number greater than 0.');
-        }
-    } else {
-        clearInterval(timer);
-        isRunning = false;
-        document.getElementById('startButton').textContent = 'Resume';
-        enableInput(); // Enable user input when the timer is paused
+        timer = setInterval(function () {
+            seconds--;
+            document.getElementById('countdown').textContent = formatTime(seconds);
+            if (seconds <= 0) {
+                clearInterval(timer);
+                isRunning = false;
+                document.getElementById('countdown').textContent = '00:00:00';
+            }
+        }, 1000);
+        isRunning = true;
     }
 }
 
-function resetTimer() {
-    clearInterval(timer);
-    seconds = 0;
-    isRunning = false;
-    document.getElementById('countdown').textContent = '00:00:00';
-    document.getElementById('startButton').textContent = 'Start';
-    enableInput(); // Enable user input when the timer is reset
-}
-
-function disableInput() {
-    document.getElementById('startButton').disabled = true;
-    document.getElementById('resetButton').disabled = true;
-}
-
-function enableInput() {
-    document.getElementById('startButton').disabled = false;
-    document.getElementById('resetButton').disabled = false;
-}
-
-document.getElementById('startButton').addEventListener('click', startTimer);
-document.getElementById('resetButton').addEventListener('click', resetTimer);
+// Start the timer automatically when the page loads
+startTimer();
