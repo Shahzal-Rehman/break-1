137a7102ad5fc9ac2a1226cb01f5b0451e8c983c
Shahzal Rehman <119114795+SR123456789-maker@users.noreply.github.com>
2023-09-17 15:01:52 +0500

    script.js

--- script.js
+++ script.js
@@ -13,22 +13,9 @@ function hideProBadge() {
 
 // Function to prompt the user for the duration
 function getDuration() {
-    const userDuration = prompt("Enter the duration in minutes (e.g., 20):");
+    let userDuration = prompt("Enter the duration in minutes (e.g., 20):");
 
     // Validate and set the duration
     if (userDuration && !isNaN(userDuration) && userDuration > 0) {
-        seconds = userDuration * 60;
-        countdownElement.textContent = formatTime(seconds);
-    } else {
-        // Use the default duration if invalid input
-        seconds = 20 * 60; // 20 minutes
-        countdownElement.textContent = formatTime(seconds);
-    }
-}
-
-// Add a call to getDuration() to prompt the user
-getDuration();
-
-// Rest of your existing JavaScript code for the timer (unchanged)
-
-// ... (existing timer code)
+        userDuration = parseInt(userDuration);
+        startTimer(userDuration * 60); // Start the timer with
