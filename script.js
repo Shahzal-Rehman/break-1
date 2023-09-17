34ac8da70ff08d4ffe30ac7149d1a765e88c59ad
Shahzal Rehman <119114795+SR123456789-maker@users.noreply.github.com>
2023-09-17 14:52:36 +0500

    script.js

--- script.js
+++ script.js
@@ -1,40 +1,47 @@
+// JavaScript for the "Pro" badge
+const proBadge = document.querySelector('.pro-badge');
+
+// Function to show the "Pro" badge
+function showProBadge() {
+    proBadge.style.display = 'block';
+}
+
+// Function to hide the "Pro" badge
+function hideProBadge() {
+    proBadge.style.display = 'none';
+}
+
+// Add a call to showProBadge() to display the badge initially
+showProBadge();
+
+// Rest of your existing JavaScript code for the timer
+const countdownElement = document.getElementById('countdown');
+const welcomeMessage = document.getElementById('welcomeMessage');
+let seconds = 20 * 60; // 20 minutes
 let timer;
-let isRunning = false;
-let seconds = 0;
+
+function updateCountdown() {
+    countdownElement.textContent = formatTime(seconds);
+    seconds--;
+
+    if (seconds < 0) {
+        clearInterval(timer);
+        document.body.style.backgroundColor = '#000';
+        // Show the welcome message when the timer reaches 0
+        welcomeMessage.style.display = 'block';
+        // Hide the "Pro" badge
+        hideProBadge();
+        return; // Leave this return statement here
+    }
+
+    const brightness = (100 - (seconds / 20) * 5) + '%';
+    document.body.style.backgroundColor = `hsl(0, 0%, ${brightness})`;
+}
 
 function formatTime(seconds) {
-    const hours = Math.floor(seconds / 3600);
-    const minutes = Math.floor((seconds % 3600) / 60);
+    const minutes = Math.floor(seconds / 60);
     const remainingSeconds = seconds % 60;
-    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
+    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
 }
 
-function startTimer() {
-    if (!isRunning) {
-        const userInput = prompt('Enter timer duration in seconds:');
-        if (userInput !== null && !isNaN(userInput) && userInput > 0) {
-            seconds = parseInt(userInput);
-            timer = setInterval(function () {
-                seconds--;
-                document.getElementById('countdown').textContent = formatTime(seconds);
-                if (seconds <= 0) {
-                    clearInterval(timer);
-                    isRunning = false;
-                    document.getElementById('countdown').textContent = '00:00:00';
-                    showWelcomeMessage(); // Display the welcome message
-                }
-            }, 1000);
-            isRunning = true;
-        } else {
-            alert('Invalid input. Please enter a valid number greater than 0.');
-        }
-    }
-}
-
-function showWelcomeMessage() {
-    const welcomeMessage = document.getElementById('welcomeMessage');
-    welcomeMessage.style.display = 'block';
-}
-
-// Start the timer when the page loads
-startTimer();
+timer = setInterval(updateCountdown, 1000); // Update countdown every second
